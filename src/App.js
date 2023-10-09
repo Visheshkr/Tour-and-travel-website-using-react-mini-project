08a2768fe426995fe5e7b645ae2a6ab1e154cfa0
Vishesh <ranavisheshsingh242@gmail.com>
2023-10-09 13:07:56 +0530

    added Router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Footer from './Components/Footer/Footer'
 import Main from './Components/Main/Main';
+import Contact from "./Components/Contact";
+import News from "./Components/News";
+import About from "./Components/About";
+import Packages from "./Components/Packages";
+import Pages from "./Components/Pages";
+import Shop from "./Components/Shop";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
     <>
+    
+    <Router>
     <Navbar/>
-    <Home/>
+      <Routes>
+      <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/packages' element={<Packages/>}/>
+        <Route exact path='/shop' element={<Shop/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/pages' element={<Pages/>}/>
+        <Route exact path='/news' element={<News/>}/>
+        <Route exact path='/contact' element={<Contact/>}/>
+        </Routes>
     <Main/>
     <Footer/>
+      </Router>
+    
     </>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import React,{useState} from 'react'
 import './navbar.css'
 import {MdOutlineTravelExplore} from 'react-icons/md'
-import {AiFillCloseCircle} from 'react-icons/ai'
+import {AiFillCloseCircle} from 'react-icons/ai';
 import {TbGridDots} from 'react-icons/tb';
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
   const[active ,setActive]=useState('navBar')
   const showNav=()=>{
     setActive('navBar activeNavbar')
   }
 
-  //function to remove Navbar
+  //function href remove Navbar
   const removeNavbar=()=>{
     setActive('navBar')
   }
@@ -17,38 +19,38 @@ const Navbar = () => {
    <section className='navBarSection'>
     <header className="header flex">
       <div className="logoDiv">
-        <a href="#" className="logo flex">
+        <Link to="/" className="logo flex">
           <h1><MdOutlineTravelExplore className="icon"/>Travel.</h1>
-        </a>
+        </Link>
       </div>
       <div className={active}>
         <ul className="navLists flex">
           <li className="navItem">
-            <a href="/" className="navLink">Home</a>
+            <Link to="/" className="navLink">Home</Link>
           </li>
 
           <li className="navItem">
-            <a href="#" className="navLink">Packages</a>
+            <Link to="/packages" className="navLink">Packages</Link>
           </li>
 
           <li className="navItem">
-            <a href="#" className="navLink">Shop</a>
+            <Link to="/shop" className="navLink">Shop</Link>
           </li>
 
           <li className="navItem">
-            <a href="#" className="navLink">About</a>
+            <Link to="/about" className="navLink">About</Link>
           </li>
 
           <li className="navItem">
-            <a href="#" className="navLink">Pages</a>
+            <Link to="/pages" className="navLink">Pages</Link>
           </li>
 
           <li className="navItem">
-            <a href="#" className="navLink">News</a>
+            <Link to="/news" className="navLink">News</Link>
           </li>
 
           <li className="navItem">
-            <a href="#" className="navLink">Contact</a>
+            <Link to="/contact" className="navLink">Contact</Link>
           </li>
 
           <button className='btn'>
